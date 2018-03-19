88cc1bd042683e777e55941dcfe0ef94e50106d8
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-03-19 06:33:50 +0100

    chore: update angular suite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'tr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Ng-Worshop seed';
-
-  constructor() { }
-
-  ngOnInit() {
-
-  }
 }
