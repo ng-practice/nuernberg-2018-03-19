379a9f441b957597c4d7cf49c4b589cd261a8abf
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-03-21 10:00:39 +0100

    http: handle errors

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,26 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { map, delay, tap } from 'rxjs/operators';
 
 import { BookShelfHttp } from './lib/book-shelf.http.service';
 import { byRating } from './lib/by-rating';
 import { Book } from './models/book';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'tr-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
-export class BooksComponent implements OnInit {
+export class BooksComponent implements OnInit, OnDestroy {
+  private _subscription: Subscription;
+
+  isBusy = true;
+  errorMessage: string;
+
   query: string;
   books: Book[];
-
+  books$: Observable<Book[]>;
   constructor(private _booksShelf: BookShelfHttp) {}
 
   ngOnInit(): void {
-    this._booksShelf
+    this.books$ = this._booksShelf
       .all()
-      .pipe(map(books => this.sort(books)))
-      .subscribe(books => (this.books = books));
+      .pipe(
+        delay(2000),
+        map(books => this.sort(books)),
+        tap(books => (this.books = books)),
+        tap(() => this.isBusy = false)
+      );
+  }
+
+  ngOnDestroy(): void {
+    /** VARIANTE 1 */
+    /** unsubscribe from stream */
+    this._subscription.unsubscribe();
   }
 
   updateSearchQueary(event: KeyboardEvent) {
@@ -40,7 +57,12 @@ export class BooksComponent implements OnInit {
   }
 
   updateBook(book: Book) {
-    console.log(book);
-    this._booksShelf.update(book).subscribe();
+    this._subscription =
+      this._booksShelf
+        .update(book)
+        .subscribe(
+          () => {},
+          err => this.errorMessage = err.message
+        );
   }
 }
--- src/app/books/lib/book-shelf.http.service.ts
+++ src/app/books/lib/book-shelf.http.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
+import { _throw } from 'rxjs/observable/throw';
+import { catchError, map } from 'rxjs/operators';
 
 import { Book } from '../models/book';
 
@@ -25,22 +26,33 @@ export class BookShelfHttp {
     return this._http
       .get<BookRaw[]>(`${this.apiEndpoint}/books`)
       .pipe(
-        map(books => books.map(book => new Book(
-          book.isbn,
-          book.title,
-          book.subtitle,
-          book.authors,
-          Math.random() * 100,
-          book.description,
-          book.rating,
-          new Date(),
-          book.cover
-        )))
+        map(books =>
+          books.map(
+            book =>
+              new Book(
+                book.isbn,
+                book.title,
+                book.subtitle,
+                book.authors,
+                Math.random() * 100,
+                book.description,
+                book.rating,
+                new Date(),
+                book.cover
+              )
+          )
+        )
       );
   }
 
+  // import { _throw } from 'rxjs/observable/throw';
   update(book: Book) {
     return this._http
-      .put(`${this.apiEndpoint}/book`, book);
+      .put(`${this.apiEndpoint}/book`, undefined)
+      .pipe(
+        catchError(err =>
+          _throw(new Error('Kritischer Fehler! API nicht erreichbar.'))
+        )
+      );
   }
 }
